eb0398fc292b2830815a04c1f449e318569559e0
Rypin <hhgfcswd@gmail.com>
2025-11-15 17:46:31 +0300

    feat: бургер меню для телефонов

---
+++ src/shared/ui/burgerWrapper.tsx
@@ -0,0 +1,53 @@
+import {
+    Sheet,
+    SheetClose,
+    SheetContent,
+    SheetFooter,
+    SheetHeader,
+    SheetTitle,
+    SheetTrigger
+} from "@/components/ui/sheet.tsx";
+import {Button} from "@/components/ui/button.tsx";
+import {cn} from "@/lib/utils.ts";
+import {Menu} from "lucide-react";
+import * as React from "react";
+
+function BurgerWrapper({children}: {children: React.ReactNode}) {
+    const isMobile = window.innerWidth < 768
+
+    return (
+        <>
+            {isMobile ? (
+                <Sheet>
+                    <SheetTrigger asChild>
+                        <Button
+                            size={"icon"}
+                            variant={'ghost'}
+                            className={cn('group p-5')}
+                        >
+                            <Menu className={cn('size-8 transition-all duration-300',
+                                'group-hover:stroke-sidebar-primary'
+                            )} />
+                        </Button>
+                    </SheetTrigger>
+                    <SheetContent side={'top'} className={cn('flex justify-center gap-5')}>
+                        <SheetHeader>
+                            <SheetTitle>Menu</SheetTitle>
+                        </SheetHeader>
+                        <div className={cn('flex flex-row flex-wrap justify-center items-center gap-15')}>
+                            {children}
+                        </div>
+                        <SheetFooter>
+                            <SheetClose asChild>
+                                <Button variant={'default'}>Close</Button>
+                            </SheetClose>
+                        </SheetFooter>
+                    </SheetContent>
+                </Sheet>
+            ) : children}
+        </>
+
+    );
+}
+
+export {BurgerWrapper};
