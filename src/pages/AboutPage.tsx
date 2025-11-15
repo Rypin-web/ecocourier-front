e502846cb9445a7a54e8f8607f7d00d70a567da9
Rypin <hhgfcswd@gmail.com>
2025-11-15 15:36:31 +0300

    feat: пока что просто стилизовал, чтобы смотреть как на белой и темной теме работает ui

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -1,14 +1,15 @@
 import {useTheme} from "@/components/themeProvider.tsx";
 import {cn} from "@/lib/utils.ts";
+import {Button} from "@/components/ui/button.tsx";
 
 function AboutPage() {
     const {setTheme} = useTheme()
 
     return (
-        <div className={cn('flex container p-5')}>
-            <button onClick={() => setTheme('system')}>system</button>
-            <button onClick={() => setTheme('dark')}>dark</button>
-            <button onClick={() => setTheme('light')}>light</button>
+        <div className={cn('flex container p-5 gap-3 justify-center items-center')}>
+            <Button variant={'default'} onClick={() => setTheme('system')}>system</Button>
+            <Button variant={'link'} onClick={() => setTheme('dark')}>dark</Button>
+            <Button variant={'ghost'} onClick={() => setTheme('light')}>light</Button>
         </div>
     );
 }
