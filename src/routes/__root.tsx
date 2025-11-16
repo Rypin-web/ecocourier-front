e8386f47ddc6d5cb106b4c930b4cacb46ff17f29
Rypin <hhgfcswd@gmail.com>
2025-11-16 18:13:34 +0300

    feat: подключение контекста

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -2,14 +2,17 @@ import {createRootRoute, Outlet} from "@tanstack/react-router";
 import {TanStackRouterDevtools} from "@tanstack/router-devtools";
 import {ThemeProvider} from "@/components/themeProvider.tsx";
 import {Header} from "@/widgets/Header.tsx";
+import {UserProvider} from "@/shared/providers/userProvider.tsx";
 
 export const Route = createRootRoute({
     component: () =>
         <ThemeProvider>
-            <Header/>
-            <main>
-                <Outlet />
-            </main>
+            <UserProvider>
+                <Header/>
+                <main>
+                    <Outlet />
+                </main>
+            </UserProvider>
             <TanStackRouterDevtools />
         </ThemeProvider>
 
