d5204429f4fe967c2efacae37f09a3176f18ff43
Rypin <hhgfcswd@gmail.com>
2025-11-16 13:39:13 +0300

    feat: типы для сервиса пользователя

---
+++ src/shared/types/apiUserServices.t.ts
@@ -0,0 +1,44 @@
+export type TUserLoginRequiredData = {
+    email: string
+    password: string
+}
+
+export type TUserGetMeResponseData = {
+    user:{
+        id: string
+        first_name: string
+        last_name: string
+        phone: string
+        email: string
+        role:string
+        createdAt: string
+        updatedAt: string
+    }
+}
+
+export type TUserLoginResponseData = {
+    user:{
+        id: string
+        first_name: string
+        last_name: string
+        phone: string
+        role:string
+        email: string
+        createdAt: string
+        updatedAt: string
+    }
+    sessionToken: string
+}
+
+export type TUserRegisterRequiredData = {
+    email: string
+    password: string
+    first_name: string
+    last_name: string
+    phone: string
+}
+
+
+export type TUserRefreshResponseData = {
+    sessionToken: string
+}
