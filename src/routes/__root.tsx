bd0cb62d043c1b0c2357b970dec7da9d404c412a
Rypin <hhgfcswd@gmail.com>
2025-11-17 19:21:10 +0300

    feat: Подключение уведомлений

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -3,6 +3,7 @@ import {TanStackRouterDevtools} from "@tanstack/router-devtools";
 import {ThemeProvider} from "@/components/themeProvider.tsx";
 import {Header} from "@/widgets/Header.tsx";
 import {UserProvider} from "@/shared/providers/userProvider.tsx";
+import {Toaster} from "@/components/ui/sonner.tsx";
 
 export const Route = createRootRoute({
     component: () =>
@@ -12,6 +13,7 @@ export const Route = createRootRoute({
                 <main>
                     <Outlet />
                 </main>
+                <Toaster position={"top-center"} />
             </UserProvider>
             <TanStackRouterDevtools />
         </ThemeProvider>
