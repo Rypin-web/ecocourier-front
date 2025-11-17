b3204333b9b1f5c61df092b0b64c2d4b18305ca5
Rypin <hhgfcswd@gmail.com>
2025-11-17 19:17:52 +0300

    feat: я тебе в рот ссал

--- eslint.config.js
+++ eslint.config.js
@@ -20,7 +20,8 @@ export default defineConfig([
       globals: globals.browser,
     },
     rules:{
-      'react-refresh/only-export-components': 'off'
+      'react-refresh/only-export-components': 'off',
+      '@typescript-eslint/no-explicit-any': 'off'
     }
   },
 ])
