2466a0de9da0fe6cfa4ba048c89e815ee7fc4c1d
Rypin <hhgfcswd@gmail.com>
2025-11-18 17:40:18 +0300

    test: просто проверка для автовхода по токену

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,29 +1,14 @@
 import {cn} from "@/lib/utils.ts";
-import {useLogin} from "@/hooks/useUserService.ts";
-import {Button} from "@/components/ui/button.tsx";
 import {useUserContext} from "@/shared/providers/userProvider.tsx";
 
 function HomePage() {
-    const userLogin = useLogin()
     const userContext = useUserContext()
 
-    if(userLogin.data && userContext) {
-        userContext.setUser(userLogin.data.data.data.user)
-    }
-
-    if(userContext?.user) {
-        console.log(userContext.user)
-    }
+    console.log('@@@@',userContext)
 
     return (
         <div className={cn('flex container p-5')}>
-            qweqweqwe
-            <Button onClick={() => userLogin.mutate({
-                email: 'marlin@gmail.com',
-                password: 'marlin1234'
-            })}>
-                ioquwioeqw
-            </Button>
+            {userContext.user?.first_name}
         </div>
     );
 }
