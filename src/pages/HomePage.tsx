f2cfe63575569bf0b503ee373c437791a500555a
Rypin <hhgfcswd@gmail.com>
2025-11-19 16:06:39 +0300

    refactor: убрал всю срань

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -4,8 +4,6 @@ import {useUserContext} from "@/shared/providers/userProvider.tsx";
 function HomePage() {
     const userContext = useUserContext()
 
-    console.log('@@@@',userContext)
-
     return (
         <div className={cn('flex container p-5')}>
             {userContext.user?.first_name}
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,23 +1,9 @@
 import {createRootRoute} from "@tanstack/react-router";
 import {Root} from "@/pages/Root.tsx";
-import {userServiceGetMe} from "@/entity/userService.ts";
-import type {TUserGetMeResponseData} from "@/shared/types/apiUserServices.t.ts";
 
 
 export const Route = createRootRoute({
-    loader: async () => {
-        try {
-            const data = await userServiceGetMe.queryFn<TUserGetMeResponseData>()
-            if (data.status < 300) {
-                console.log('@@Succes request')
-                return {userData: data.data.data.user}
-            }
-        } catch {
-            return {userData: null}
-        }
-        return {userData: null}
-    },
     component: () => {
-        return <Root user={null} />
+        return <Root />
     },
 })
