2bd22e863e4a4c6dc988677f3a46848a93fe6e38
Rypin <hhgfcswd@gmail.com>
2025-11-19 17:16:27 +0300

    feat: навигация

---
+++ src/shared/ui/Navigation.tsx
@@ -0,0 +1,35 @@
+import {Item, ItemContent} from "@/components/ui/item.tsx";
+import {Button} from "@/components/ui/button.tsx";
+import {Link} from "@tanstack/react-router";
+import {cn} from "@/lib/utils.ts";
+
+function Navigation() {
+    return (
+        <Item variant={'default'}>
+            <ItemContent
+                className={window.innerWidth > 937
+                    ? cn('flex flex-row justify-center ml-[-4rem] mr-[-3rem]')
+                    : cn('flex flex-row justify-center')
+                }
+            >
+                <Button asChild variant={'link'} >
+                    <Link to={'/about'}  className={cn('text-[1.2rem] font-bold')}>
+                        О нас
+                    </Link>
+                </Button>
+                <Button asChild variant={'link'}>
+                    <Link to={'/about'} className={cn('text-[1.2rem] font-bold')}>
+                        Партнеры
+                    </Link>
+                </Button>
+                <Button asChild variant={'link'}>
+                    <Link to={'/about'}  className={cn('text-[1.2rem] font-bold')}>
+                        Новости
+                    </Link>
+                </Button>
+            </ItemContent>
+        </Item>
+    );
+}
+
+export {Navigation};
