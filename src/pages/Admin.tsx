a823c0bc6e567fd8c654a01782b23e4e937169a3
Rypin <hhgfcswd@gmail.com>
2025-12-04 18:48:48 +0300

    feat: подключение навигации и правки в отступах

--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -1,9 +1,18 @@
+import {useState} from "react";
+import {AdminNavigation} from "@/widgets/AdminNavigation.tsx";
+
 function Admin() {
+    const [activeModel, setActiveModel] = useState('');
+
+    console.log(activeModel)
 
     return (
-        <div>
-            Admin
-        </div>
+        <>
+            <AdminNavigation setActiveModel={setActiveModel} />
+            <div className={'pl-[220px]'}>
+                Admin
+            </div>
+        </>
     );
 }
 
