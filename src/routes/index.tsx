225e7c94b824c8538f9a8b5d647c267b287e6908
Rypin <hhgfcswd@gmail.com>
2025-12-04 21:25:17 +0300

    fix: создал страницу '/' чтобы все норм было для tsr

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,7 @@
 import {createFileRoute} from "@tanstack/react-router";
 import {HomePage} from "@/pages/HomePage.tsx";
 
+
 export const Route = createFileRoute("/")({
-    component: ()=> <HomePage />
+    component: () => <HomePage />
 })
