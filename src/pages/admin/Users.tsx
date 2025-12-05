28a69f3591c9c4b8a0f5335fcf5d1e5eeddca92d
Rypin <hhgfcswd@gmail.com>
2025-12-05 21:41:01 +0300

    feat: таблицу сделал

--- src/pages/admin/Users.tsx
+++ src/pages/admin/Users.tsx
@@ -1,6 +1,8 @@
 import {useState} from "react";
 import type {TUserSearchParams} from "@/shared/types/apiUserServices.t.ts";
 import {useGetUsers} from "@/shared/hooks/useUserService.ts";
+import {Table, TableBody, TableHead, TableHeader, TableRow} from "@/components/ui/table.tsx";
+import {DataTableRow} from "@/widgets/DataTableRow.tsx";
 
 
 function Users() {
@@ -15,9 +17,28 @@ function Users() {
 
     return (
         <div>
-            {data?.status && <h1>{data.data.data.total}</h1>}
+            <Table>
+                <TableHeader>
+                    <TableRow>
+                        <TableHead>Индекс</TableHead>
+                        <TableHead>Роль</TableHead>
+                        <TableHead>Имя</TableHead>
+                        <TableHead>Фамилия</TableHead>
+                        <TableHead>Почта</TableHead>
+                        <TableHead>Номер</TableHead>
+                        <TableHead>Создано:</TableHead>
+                        <TableHead>Обновлено:</TableHead>
+                    </TableRow>
+                </TableHeader>
+                <TableBody>
+                    {/*TODO: Обернуть компонент в итем и намутить диалог для изменения данных.
+                    TODO Ну туда можно и удаление вставить*/}
+                    {data?.data.data.users.map((e, index) => <DataTableRow index={index} data={e} />)}
+                </TableBody>
+            </Table>
         </div>
     );
 }
+//TODO: Не забыть про создание пользователя
 
 export {Users};
