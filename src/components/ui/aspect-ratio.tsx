3b4d124f5ca283476de417924b6a0aa68606cf04
Rypin <hhgfcswd@gmail.com>
2025-12-06 23:03:20 +0300

    deps: комопненты shadcn/ui

---
+++ src/components/ui/aspect-ratio.tsx
@@ -0,0 +1,9 @@
+import * as AspectRatioPrimitive from "@radix-ui/react-aspect-ratio"
+
+function AspectRatio({
+  ...props
+}: React.ComponentProps<typeof AspectRatioPrimitive.Root>) {
+  return <AspectRatioPrimitive.Root data-slot="aspect-ratio" className='bg-muted rounded-xl' {...props} />
+}
+
+export { AspectRatio }
---
+++ src/components/ui/skeleton.tsx
@@ -0,0 +1,13 @@
+import { cn } from "@/shared/utils/cn"
+
+function Skeleton({ className, children = '', ...props }: React.ComponentProps<"div">) {
+  return (
+    <div
+      data-slot="skeleton"
+      className={cn("bg-accent animate-pulse", className)}
+      {...props}
+    >{children}</div>
+  )
+}
+
+export { Skeleton }
