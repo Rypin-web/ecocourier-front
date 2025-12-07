d1ba1dc0cdab2850082126fc348c0e3192d6d6a5
Rypin <hhgfcswd@gmail.com>
2025-12-07 19:11:15 +0300

    feat: скелетон для пагинации

---
+++ src/features/PaginationSkeleton.tsx
@@ -0,0 +1,44 @@
+import {
+    Pagination,
+    PaginationContent,
+    PaginationEllipsis,
+    PaginationItem,
+    PaginationNext, PaginationPrevious
+} from "@/components/ui/pagination.tsx";
+import {Skeleton} from "@/components/ui/skeleton.tsx";
+
+function PaginationSkeleton() {
+    return (
+        <Pagination>
+            <PaginationContent>
+                <PaginationItem>
+                    <Skeleton>
+                        <PaginationPrevious>Назад</PaginationPrevious>
+                    </Skeleton>
+                </PaginationItem>
+                <PaginationItem>
+                    <Skeleton>
+                        <PaginationEllipsis className='stroke-white/30' />
+                    </Skeleton>
+                </PaginationItem>
+                {new Array(10).fill(0).map(() => (
+                    <PaginationItem>
+                        <Skeleton className='w-9 h-9' />
+                    </PaginationItem>
+                ))}
+                <PaginationItem>
+                    <Skeleton>
+                        <PaginationEllipsis className='stroke-white/30' />
+                    </Skeleton>
+                </PaginationItem>
+                <PaginationItem>
+                    <Skeleton>
+                        <PaginationNext>Вперед</PaginationNext>
+                    </Skeleton>
+                </PaginationItem>
+            </PaginationContent>
+        </Pagination>
+    );
+}
+
+export {PaginationSkeleton};
