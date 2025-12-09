69f08b76341d287153439c204ab7ff505f27d801
Rypin <hhgfcswd@gmail.com>
2025-12-09 19:12:50 +0300

    refactor: заменил на текст на компоненты

--- src/features/WeUseCookie.tsx
+++ src/features/WeUseCookie.tsx
@@ -1,12 +1,13 @@
 import {useEffect, useState} from "react";
 import {Button} from "@/components/ui/button";
 import {Card, CardContent} from "@/components/ui/card";
+import {TypographyH2, TypographyLarge, TypographyP} from "@/components/ui/typography.tsx";
 
 function WeUseCookie() {
     const [visible, setVisible] = useState(false);
 
     useEffect(() => {
-        const hasCookie = localStorage.getItem("use-cookie");
+        const hasCookie = parseInt(localStorage.getItem("use-cookie") || '0');
         if (!hasCookie) setVisible(true);
     }, []);
 
@@ -19,10 +20,13 @@ function WeUseCookie() {
 
     return (
         <div className='fixed bottom-4 translate-x-1/20 z-50'>
-            <Card className='w-[90vw] shadow-xl p-4 rounded-2xl'>
+            <Card className='w-[90vw] shadow-xl p-10 rounded-2xl'>
                 <CardContent className='flex flex-col gap-3 p-0'>
-                    <h1 className='text-lg bold'>Мы используем cookies</h1>
-                    <Button onClick={accept} className='rounded-xl w-3xs self-end'>Хорошо</Button>
+                    <TypographyH2 className={'text-left'}>Мы используем куки</TypographyH2>
+                    <TypographyP>С их помощью сайт запоминает информацию о ваших посещениях.</TypographyP>
+                    <Button onClick={accept} className='rounded-xl w-3xs self-end'>
+                        <TypographyLarge>Хорошо</TypographyLarge>
+                    </Button>
                 </CardContent>
             </Card>
         </div>
