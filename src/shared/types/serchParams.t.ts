5cdebc700c5b8a6e642bef2d332c75d76c261dd0
Rypin <hhgfcswd@gmail.com>
2025-12-09 17:29:44 +0300

    feat: типизация параметра sortBy

--- src/shared/types/serchParams.t.ts
+++ src/shared/types/serchParams.t.ts
@@ -1,5 +1,6 @@
-export type TUserSearchParams = {
+export type TUserSearchParams<T> = {
     limit: number
     page: number
     sort: 'ASC' | 'DESC'
+    sortBy: 'createdAt' | 'updatedAt' | 'id' | T
 }
