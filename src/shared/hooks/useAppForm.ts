a7fc133e4a3ee0404c4a7dea9ad7a33c30cc9bae
Rypin <hhgfcswd@gmail.com>
2026-01-01 23:55:51 +0300

    feat: эээ забыл еще подключить его

--- src/shared/hooks/useAppForm.ts
+++ src/shared/hooks/useAppForm.ts
@@ -1,12 +1,14 @@
 import {createFormHook, createFormHookContexts} from "@tanstack/react-form";
 import {FormInput} from "@/shared/ui/formInput.tsx";
 import {SubmitButton} from "@/shared/ui/submitButton.tsx";
+import {FormDnd} from "@/shared/ui/FormDnd.tsx";
 
 const {fieldContext, formContext} = createFormHookContexts()
 
 export const {useAppForm} = createFormHook({
     fieldComponents: {
-        FormInput
+        FormInput,
+        FormDnd: FormDnd
     },
     formComponents: {
         SubmitButton
