ddd0403f9ff4ce65ca44ec3cc8d77d5a2979b1ae
Rypin <hhgfcswd@gmail.com>
2026-02-22 15:03:01 +0300

    feat: кнопка с активной таблицей подсвечивается

--- src/widgets/AdminNavigation/AdminNavigationItem.tsx
+++ src/widgets/AdminNavigation/AdminNavigationItem.tsx
@@ -1,32 +1,43 @@
 import {Item, ItemActions, ItemContent, ItemTitle} from "@/components/ui/item.tsx";
 import {ExternalLinkIcon} from "lucide-react";
-import type {ReactNode} from "react";
-import {Link} from "@tanstack/react-router";
+import {type ReactNode} from "react";
+import {Link, useRouterState} from "@tanstack/react-router";
+import {cn} from "@/shared/utils/cn.ts";
 
 interface AdminNavigationItemProps {
-    children: ReactNode | string,
-    modelName: string
+  children: ReactNode | string,
+  modelName: string
 }
 
 function AdminNavigationItem({children, modelName,}: AdminNavigationItemProps) {
-    const handleClick = (name:string) => {
-        localStorage.setItem('selectedAdminTable', `/${name}`)
-    }
+  const handleClick = (name: string) => {
+    localStorage.setItem('selectedAdminTable', `/${name}`)
+  }
+  const currentPath = useRouterState({
+    select: state => state.location.pathname
+  })
 
-    return (
-        <Item asChild variant={'outline'} className={'mb-3'}>
-            <Link to={'/admin/' + modelName} onClick={() => handleClick(modelName)}>
-                <ItemContent>
-                    <ItemTitle className={'text-lg'}>
-                        {children}
-                    </ItemTitle>
-                </ItemContent>
-                <ItemActions>
-                    <ExternalLinkIcon className={'size-4'} />
-                </ItemActions>
-            </Link>
-        </Item>
-    );
+  return (
+    <Item asChild variant={'outline'}
+      className={cn(
+        'mb-3',
+        currentPath === '/admin/' + modelName ? 'bg-accent' : ''
+      )}
+    >
+      <Link
+        to={'/admin/' + modelName}
+        onClick={() => handleClick(modelName)}>
+        <ItemContent>
+          <ItemTitle className={'text-lg'}>
+            {children}
+          </ItemTitle>
+        </ItemContent>
+        <ItemActions>
+          <ExternalLinkIcon className={'size-4'} />
+        </ItemActions>
+      </Link>
+    </Item>
+  );
 }
 
 export {AdminNavigationItem};
