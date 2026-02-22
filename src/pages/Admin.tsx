50c5969acd199d8635a7f0fd8d41441f19689513
Rypin <hhgfcswd@gmail.com>
2026-02-22 15:02:36 +0300

    feat: таблицы теперь перед загрузкой показывают загрузку

--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -1,16 +1,24 @@
 import {AdminNavigation} from "@/widgets/AdminNavigation/AdminNavigation.tsx";
 import {Outlet} from "@tanstack/react-router";
+import {LoadingProvider} from "@/shared/providers/LoadingProvider.tsx";
 
 function Admin() {
 
-    return (
-        <>
-            <AdminNavigation />
-            <div className={'pl-[250px] pt-[70px]'}>
-                <Outlet />
-            </div>
-        </>
-    );
+  return (
+    <>
+      <AdminNavigation />
+      <div className={'pl-[250px] pt-[70px]'}>
+        <LoadingProvider path={[
+          '/admin/users',
+          '/admin/products',
+          '/admin/categories'
+        ]}>
+          <Outlet />
+        </LoadingProvider>
+
+      </div>
+    </>
+  );
 }
 
 export {Admin};
