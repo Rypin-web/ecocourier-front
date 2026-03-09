03153d01c0169cb5d2c145023b20233f0aadb160
Rypin <hhgfcswd@gmail.com>
2026-03-09 17:20:10 +0300

    feat: добавил валидацию для форматов картинок

--- src/shared/ui/FormDnd.tsx
+++ src/shared/ui/FormDnd.tsx
@@ -6,6 +6,7 @@ import {AspectRatio} from "@/components/ui/aspect-ratio.tsx";
 import {apiUrl} from "@/shared/constants/api.ts";
 import {cn} from "@/shared/utils/cn.ts";
 import {CameraOff, ExternalLink} from "lucide-react";
+import {useEffect} from "react";
 
 interface FormInputProps {
   field: FieldApi<any, any, any, any, any, any, any, any, any, any, any, any, any, any, any, any, any, any, any, any, any, any, any>
@@ -15,23 +16,32 @@ interface FormInputProps {
 function FormDnd({field, image}: FormInputProps) {
   const {acceptedFiles, getInputProps, getRootProps, isDragAccept, isDragReject} = useDropzone({
       onDrop: (files) => {
+        const validType = ['.png', '.jpeg', '.jpg']
+        if (file && !validType.includes(files[0].type)) return
         field.handleChange(() => files[0])
       },
       multiple: false,
       accept: {
-        'image/*': ['.png', '.jpeg', '.jpg']
-      }
+        'image/png': ['.png'],
+        'image/jpeg': ['.jpeg', '.jpg']
+      },
+      noClick: true,
+      noKeyboard: true
     }
   )
   const file = acceptedFiles[0]
+
+  useEffect(() => {
+    field.handleChange(() => image)
+  }, [isDragReject])
 
   return (
     <div className='size-70 mx-auto mb-[40px] text-center'>
       <div {...getRootProps()} className={cn(
         'rounded-xl p-1 border-3 border-dashed border-transparent outline-none transition-all duration-200 ease-in-out',
         'hover:p-5 hover:border-chart-5/50 focus:p-5 focus:border-chart-5/50',
-        isDragAccept && 'border-primary',
         isDragReject && 'border-destructive p-5',
+        isDragAccept && 'border-primary',
         !!file && !isDragAccept && !isDragReject ? 'border-primary/40' : ''
       )}>
         <Input name={field.name} {...getInputProps()} />
@@ -39,8 +49,8 @@ function FormDnd({field, image}: FormInputProps) {
           <>
             <AspectRatio className={cn(
               'z-2 opacity-80 size-[100%] transition-all duration-200 ease-in-out',
+              isDragReject && 'animate-wiggle',
               isDragAccept && 'size-30 translate-y-[10%] translate-x-[15%]',
-              isDragReject && 'animate-wiggle'
             )} ratio={1 / 1}>
               {
                 !image || image === 'Unset'
